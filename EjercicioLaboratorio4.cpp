e76fb2a18a020ff07665ad6b8ba2e1babdb82345
Marcos Noe Vasquez Guardado <00114825@uca.edu.sv>
2025-04-28 08:12:18 -0600

    Update EjercicioLaboratorio4.cpp

--- EjercicioLaboratorio4.cpp
+++ EjercicioLaboratorio4.cpp
@@ -5,22 +5,26 @@ using namespace std;
 int main()
 {
 
-    // Declare variable : old
+    // Declare variable : old - Declarar la variable edad
     int old = 0;
 
-    // Introduce the old
+    // Introduce the old - Introduce la edad
     cout << "Let introduce your old" << endl;
     cin >> old;
 
-    // Verify old
+    // Verify payment based on age using conditionals - Verificar el pago dependiendo de la edad mediante condicionales
+
+    // Si la edad es mayor a 5 , la entrada es gratis
     if (old < 5)
     {
         cout << "Children can enter for free";
     }
+    //Si la edad es igual o mayor a 5 y menor a 12 años la entrada es de $5
     else if (old >= 5 && old <= 12)
     {
         cout << "Admission is for children $5";
     }
+     //Si la edad es mayor a 12 años , la entrada es de $10
     else if (old > 12)
     {
         cout << "Admission is for children $10";
